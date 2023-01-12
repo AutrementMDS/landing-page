34d09df88036ee9419a8c59c511abfafabce7428
Justin A-M <justin@omnibook.com>
2023-01-12 16:24:56 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/CGU", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/CGU.html"));
+});
+
+app.get("/cookies", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/cookies.html"));
+});
+
+app.get("/confidentialite", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/confidentialite.html"));
+});
+
+app.listen(process.env.PORT, () => console.log("listening on port 3000"));
